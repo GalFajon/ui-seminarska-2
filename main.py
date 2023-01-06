2139a5be510c2ddcd9b66f3d6951f4cb13d06c98
GalFajon <gla.nojaf@gmail.com>
2023-01-06 16:40:00 +0100

    Final update.

--- main.py
+++ main.py
@@ -12,11 +12,19 @@ for i,case in enumerate(tests.cases):
     w2 = warehouse.Warehouse(case['p'],case['n'],tuple([ tuple(arr) for arr in case['end']] ))
     n2 = graph.Node([],[],w2)
 
+    # DFS:
     #search.dfs(g,n2)
+    
+    # BFS:
     #search.bfs(g,n2)
+    
+    # ID:
     #search.id(g,n2)
-    ida = idastar.IdaStar(g,n2)
-    ida.find()
-
+    
+    # IDA STAR:
+    #ida = idastar.IdaStar(g,n2)
+    #ida.find()
     #search.idastar(g,n2)
-    search.astar(g,n2)
+
+    # ASTAR:
+    #search.astar(g,n2)

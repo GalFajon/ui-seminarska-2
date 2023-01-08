d0bab8c989167aa3ac69d085f1002c6d29d3af51
GalFajon <gla.nojaf@gmail.com>
2023-01-08 18:33:40 +0100

    Added timers to searches.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import graph;
 import search;
 import tests;
 
-for i,case in enumerate(tests.cases):
+import time
+
+for i,case in enumerate([ tests.cases[4] ]):
     print('------------------')
     print('Testni primer: ', i)
     print('------------------')
@@ -13,18 +15,28 @@ for i,case in enumerate(tests.cases):
     w2 = warehouse.Warehouse(case['p'],case['n'],tuple([ tuple(arr) for arr in case['end']] ))
     n2 = graph.Node([],[],w2)
 
-    print("# DFS:")
-    search.dfs(g,n2)
+    #print("# DFS:")
+    #start = time.time()
+    #search.dfs(g,n2)
+    #print("cas izvedbe: ", time.time() - start)
 
     #print("# BFS:")
+    #start = time.time()
     #search.bfs(g,n2)
+    #print("cas izvedbe: ", time.time() - start)
 
     #print("# ID:")
+    #start = time.time()
     #search.id(g,n2)
+    #print("cas izvedbe: ", time.time() - start)
 
     #print("# A STAR:")
+    #start = time.time()
     #search.aStar.search(g,n2)
+    #print("cas izvedbe: ", time.time() - start)
     
-    #print("# IDA STAR:")
-    #ida = search.IdaStar(g,n2)
-    #ida.find()
+    print("# IDA STAR:")
+    start = time.time()
+    ida = search.IdaStar(g,n2)
+    ida.find()
+    print("cas izvedbe: ", time.time() - start)
